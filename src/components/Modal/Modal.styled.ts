9b0076f713f1fbb8affa8a2afb5962e01a530718
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-08-19 18:27:32 +0100

    fix modal image size

--- src/components/Modal/Modal.styled.ts
+++ src/components/Modal/Modal.styled.ts
@@ -1,14 +1,23 @@
 import styled from "styled-components";
 
 export const Overlay = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
   backdrop-filter: blur(3px);
   z-index: 5;
+
+  & img {
+    width: auto;
+    height: auto;
+    max-width: 100%;
+    max-height: 80vh;
+  }
 `;
 
 export const CloseBtn = styled.button`
